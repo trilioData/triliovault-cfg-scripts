36abe70404a805304077a01152dba4d8cee53f4f
shyam-biradar <shyam.biradar@trilio.io>
2018-05-08 18:59:16 +0530

    Adding puppet module to install datamover extension

---
+++ redhat-integration/puppet/trilio/manifests/contego/contego_install.pp
@@ -0,0 +1,34 @@
+class trilio::contego::contego_install inherits trilio::contego {
+  
+
+    notify { $openstack_release: } 
+## Adding nova user to system groups
+    user { 'Add_nova_user_to_system_groups':
+        name   => $contego_user,
+        ensure => present,
+        gid    => $contego_group,
+        groups => $contego_groups,
+    }->
+    file { "/home/tvault/":
+        ensure => 'directory',
+        owner  => $contego_user,
+        group  => $contego_group,
+    }
+
+
+    file { '/tmp/contego_install.sh':
+        ensure  => 'present',
+        owner   => root,
+        group   => root,
+	mode    => '0711',
+	source  => 'puppet:///modules/trilio/contego_install.sh',
+        require => File["/home/tvault"]
+    }->
+    exec { 'install_upgrade_datamover':
+        command  => "/tmp/contego_install.sh ${contego_dir} ${tvault_appliance_ip} ${openstack_release} > /tmp/contego_install.log",
+        provider => shell,
+        path     => ['/bin/bash','/usr/bin','/usr/sbin','usr/local/bin'],
+        onlyif   => '/usr/bin/test -e /tmp/contego_install.sh',
+    }
+  
+}
