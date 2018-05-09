7e8c9fb1da01f852d9f9f03edfbb725daf8ff5c2
shyam-biradar <shyam.biradar@trilio.io>
2018-05-09 18:58:57 +0530

    Adding playbook to edploy tvault-contego-api package

--- redhat-integration/puppet/trilio/manifests/api.pp
+++ redhat-integration/puppet/trilio/manifests/api.pp
@@ -1,24 +1,24 @@
 class trilio::api {
 
-
-    Exec { environment => [ "TVAULT_PACKAGE=tvault-contego-api" ] }
-    exec { 'Install pip':
-        command => 'easy_install --no-deps http://192.168.1.26:8081/packages/pip-7.1.2.tar.gz',
-        path    => ['/usr/bin/','/usr/sbin/', '/usr/local/bin/'],
-        unless  => 'export PIP_INS=`pip --version || true`;  $PIP_INS != pip* '
+    package {'python-pip':
+        ensure   => present,
+        provider => yum,
     }
-    exec { 'Installapipackage':
-        command => 'pip install --no-deps http://192.168.1.26:8081/packages/tvault-contego-api-3.0.5.tar.gz',
-        path    => ['/usr/bin/','/usr/sbin/', '/usr/local/bin/'],
-    }    
 
-#    package {'dummy':
-#        ensure   => present,
-#        provider => pip,
-   #     install_options => [ {'-f' => ' http://192.168.1.26:8081/packages/' }, 
-                                   #{'--trusted-host' => '192.168.1.26'} ],	
-	 
-#	source   => "http://192.168.1.26:8081/packages/",
-#    }
+    package {'tvault-contego-api':
+        ensure   => present,
+        provider => pip,
+        source   => "http://192.168.1.26:8081/packages/tvault-contego-api-${contego_version}.tar.gz",
+        require  => Package['python-pip'],
+        notify   => Service['openstack-nova-api']
+    }
+
+    service { 'openstack-nova-api':
+        ensure      => running,
+        enable      => true,
+        hasstatus   => true,
+        hasrestart  => true,
+    }
+ 
 
 }
