278da30bf5dfe47076ae8f30107eb0f7ab31c0ea
shyam-biradar <shyam.biradar@trilio.io>
2018-05-09 18:58:57 +0530

    Adding playbook to edploy tvault-contego-api package

---
+++ redhat-integration/puppet/trilio/lib/facter/env.rb
@@ -0,0 +1,6 @@
+Facter.add(:contego_version) do
+  setcode do
+  version = Facter::Core::Execution::exec("/usr/bin/curl -s http://192.168.1.26:8081/packages/ | grep tvault-contego-[0-9] | awk -F 'tvault-contego-' '{print $2}' | cut -c-5")
+  version
+  end
+end
