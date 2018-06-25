c35812fbcd215e5781300b98500feda97b55e274
shyam-biradar <shyam.biradar@trilio.io>
2018-06-25 05:06:59 -0400

    Added example conf files for nfs and s3

--- redhat-director-scripts/prepare_roles_data.py
+++ redhat-director-scripts/prepare_roles_data.py
@@ -1,5 +1,5 @@
 import yaml, sys
-
+import os
 
 role_data_file = sys.argv[1]
 
@@ -19,6 +19,9 @@ for role in roles:
         if 'OS::TripleO::Services::TrilioDatamover' not in compute_service_list:
             compute_service_list.append('OS::TripleO::Services::TrilioDatamover')
         role['ServicesDefault'] = compute_service_list
-		 
-with open(role_data_file, "w") as f:
+
+new_roles_data_file = os.path.dirname(os.path.realpath(__file__)) + "/roles_data.yaml"
+with open(new_roles_data_file, "w") as f:
     yaml.dump(roles, f)
+
+print ("\nNew roles data file is created at: %s\nTo install trilio components on overcloud, you need to use this new roles data file\n" % new_roles_data_file)
