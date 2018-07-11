8f1e2ff047b168715696e98a74d37c6c7d9b5a56
shyam-biradar <shyam.biradar@trilio.io>
2018-07-11 18:20:47 +0530

    Uncommented nova-api notify

--- redhat-director-scripts/puppet/trilio/manifests/api.pp
+++ redhat-director-scripts/puppet/trilio/manifests/api.pp
@@ -6,7 +6,7 @@ class trilio::api (
     package { 'tvault-contego-api':
         ensure   => latest,
         provider => 'yum',
-#        notify   => Service['nova-api'],
+        notify   => Service['nova-api'],
     }
 
 
