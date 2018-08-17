aa203243cb7efc5a153d01519c271788ad3d938b
shyam-biradar <shyam.biradar@trilio.io>
2018-08-17 07:37:08 -0400

    Added changes CMD and Entrypoint section of dockerfile

--- redhat-director-scripts/docker/trilio-datamover/trilio-datamover-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-/home/tvault/.virtenv/bin/python /home/tvault/.virtenv/bin/tvault-contego --config-file=/usr/share/nova/nova-dist.conf \
---config-file=/etc/nova/nova.conf --config-file=/etc/tvault-contego/tvault-contego.conf
