7acfcc2c052989c5966c7092f086ac822851070d
Shyam Biradar <shyam.biradar@trilio.io>
2018-09-07 03:23:35 -0400

    Fixed env issues

--- redhat-director-scripts/prepare_trilio_images.sh
+++ redhat-director-scripts/prepare_trilio_images.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-docker pull docker.io/trilio/trilio-datamover:ditest
+docker pull docker.io/trilio/trilio-datamover:queens
 
-docker tag docker.io/trilio/trilio-datamover:ditest 192.168.122.151:8787/trilio/trilio-datamover:ditest
+docker tag docker.io/trilio/trilio-datamover:queens 192.168.122.151:8787/trilio/trilio-datamover:queens
 
-docker push 192.168.122.151:8787/trilio/trilio-datamover:ditest
+docker push 192.168.122.151:8787/trilio/trilio-datamover:queens
 
-docker pull docker.io/trilio/trilio-datamover-api:ditest
+docker pull docker.io/trilio/trilio-datamover-api:queens
 
-docker tag docker.io/trilio/trilio-datamover-api:ditest 192.168.122.151:8787/trilio/trilio-datamover-api:ditest
+docker tag docker.io/trilio/trilio-datamover-api:queens 192.168.122.151:8787/trilio/trilio-datamover-api:queens
 
-docker push 192.168.122.151:8787/trilio/trilio-datamover-api:ditest
+docker push 192.168.122.151:8787/trilio/trilio-datamover-api:queens
