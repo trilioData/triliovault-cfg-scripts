fc1516a40e5bd1cd45d92700b6c6d99a075f6ade
Shyam Biradar <shyam.biradar@trilio.io>
2018-09-08 00:18:47 -0400

    Added code to enable ssl on dmapi

--- redhat-director-scripts/puppet/trilio/manifests/dmapi.pp
+++ redhat-director-scripts/puppet/trilio/manifests/dmapi.pp
@@ -1,7 +1,7 @@
 class trilio::dmapi ( 
   $dmapi_port                      = '8784',
   $dmapi_link_prefix               = undef,
-  $is_ssl_enabled                  = hiera('nova::wsgi::apache_api::ssl', false),
+  $dmapi_enable_ssl                = true,
   $oslomsg_rpc_proto       	   = hiera('messaging_rpc_service_name', 'rabbit'),
   $oslomsg_rpc_hosts       	   = any2array(hiera('rabbitmq_node_names', undef)),
   $oslomsg_rpc_password    	   = hiera('nova::rabbit_password'),
