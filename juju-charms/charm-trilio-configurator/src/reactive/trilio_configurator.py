9f03c01cb2b8dfd6d6242b54060f1566d00924e4
Abhijeet Patra <abhijeet.patra@trilio.io>
2018-11-28 10:46:33 -0500

    Fixed pep8 errors.

--- juju-charms/charm-trilio-configurator/src/reactive/trilio_configurator.py
+++ juju-charms/charm-trilio-configurator/src/reactive/trilio_configurator.py
@@ -1,19 +1,20 @@
-from charms.reactive import when, when_not, set_flag
+from charms.reactive import when, set_flag
 from charmhelpers.contrib import ansible
 from charmhelpers.core.hookenv import (
     status_set,
     log,
 )
 
-@when_not('trilio-configurator.installed')
+
+@when('config.changed')
 def install_configurator():
     log("Starting configuration...")
     status_set('maintenance', 'configuring tvault...')
     try:
         ansible.apply_playbook('site.yaml')
         status_set('active', 'Ready...')
-        set_flag('trilio-configurator.installed')
     except Exception as e:
         log("ERROR:  {}".format(e))
         log("Check the ansible log in TVault to find more info")
         status_set('blocked', 'configuration failed')
+    set_flag('trilio-configurator.installed')
