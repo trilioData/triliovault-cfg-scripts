c2308cee4a70c6a00ea3e229030c91731f22843c
Shyam Biradar <shyam.biradar@trilio.io>
2019-01-31 07:57:34 -0500

    TVAULT-3022

--- redhat-director-scripts/puppet/trilio/manifests/contego.pp
+++ redhat-director-scripts/puppet/trilio/manifests/contego.pp
@@ -16,7 +16,7 @@ class trilio::contego (
     $contego_group                        = 'nova'
     $contego_conf_file                    = "/etc/tvault-contego/tvault-contego.conf"
     $contego_groups                       = ['kvm','qemu','disk']
-    $vault_data_dir                       = "/var/triliovault-mounts"
+    $vault_data_dir                       = "/var/lib/nova/triliovault-mounts"
     $vault_data_dir_old                   = "/var/triliovault"
     $contego_dir                          = "/home/tvault"
     $contego_virtenv_dir                  = "${contego_dir}/.virtenv"
