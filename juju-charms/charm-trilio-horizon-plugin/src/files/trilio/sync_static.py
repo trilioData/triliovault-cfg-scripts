1851bd9d0aa502a09a812582559088fb729d5130
Abhijeet Patra <abhijeet.patra@trilio.io>
2019-12-18 12:40:01 +0000

    TVAULT-3494: Python location and version changes

--- juju-charms/charm-trilio-horizon-plugin/src/files/trilio/sync_static.py
+++ juju-charms/charm-trilio-horizon-plugin/src/files/trilio/sync_static.py
@@ -1,3 +1,4 @@
+import sys
 import settings
 import subprocess
 ls = settings.INSTALLED_APPS
@@ -5,7 +6,6 @@ data = ""
 for app in ls:
     if app != 'dashboards':
         data += "-i "+str(app)+" "
-
-subprocess.call(
-    "/usr/share/openstack-dashboard/manage.py collectstatic --noinput "+data,
-    shell=True)
+cmd = "{} /usr/share/openstack-dashboard/manage.py collectstatic"\
+      " --noinput {}".format(sys.executable, data)
+subprocess.call(cmd, shell=True)
