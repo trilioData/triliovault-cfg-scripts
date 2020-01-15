d37ea50cc0b8b0d4ca7ac600501b3611184de32d
shyam-biradar <shyam.biradar@trilio.io>
2020-01-15 05:23:02 -0500

    Added code to build containers for rhosp16

--- redhat-director-scripts/docker/build_containers.sh
+++ redhat-director-scripts/docker/build_containers.sh
@@ -194,5 +194,47 @@ podman push docker.io/trilio/trilio-horizon-plugin:${tvault_version}-rhosp15
 
 # Clean the build_dir
 rm -rf $base_dir/${build_dir}
+#############################################
 
-################# Create containers for RHOSP14 #######################
+################# Create containers for RHOSP16 #######################
+## Work dir preparation for for rhosp16
+build_dir=tmp_docker_rhosp16_${tvault_version}
+rm -rf $base_dir/${build_dir}
+mkdir -p $base_dir/${build_dir}
+
+cp -R $base_dir/trilio-datamover $base_dir/${build_dir}/
+cp -R $base_dir/trilio-datamover-api $base_dir/${build_dir}/
+cp -R $base_dir/trilio-horizon-plugin $base_dir/${build_dir}/
+
+#Build trilio-datamover containers for rhosp16
+
+echo -e "Creating trilio-datamover container for rhosp15"
+cd $base_dir/${build_dir}/trilio-datamover/
+rm Dockerfile
+cp Dockerfile_rhosp16 Dockerfile
+buildah bud -t docker.io/trilio/trilio-datamover:${tvault_version}-rhosp16 .
+podman push docker.io/trilio/trilio-datamover:${tvault_version}-rhosp16
+
+
+
+#Build trilio_datamover-api containers for rhosp16
+
+echo -e "Creating trilio-datamover-api container for rhosp16"
+cd $base_dir/${build_dir}/trilio-datamover-api/
+rm Dockerfile
+cp Dockerfile_rhosp16 Dockerfile
+buildah bud -t docker.io/trilio/trilio-datamover-api:${tvault_version}-rhosp16 .
+podman push docker.io/trilio/trilio-datamover-api:${tvault_version}-rhosp16
+
+## Build horizon plugin container for rhosp16
+
+echo -e "Creating trilio horizon plugin container for rhosp16"
+cd $base_dir/${build_dir}/trilio-horizon-plugin/
+rm Dockerfile
+cp Dockerfile_rhosp16 Dockerfile
+buildah bud -t docker.io/trilio/trilio-horizon-plugin:${tvault_version}-rhosp16 .
+podman push docker.io/trilio/trilio-horizon-plugin:${tvault_version}-rhosp16
+
+# Clean the build_dir
+rm -rf $base_dir/${build_dir}
+#########################################################################
