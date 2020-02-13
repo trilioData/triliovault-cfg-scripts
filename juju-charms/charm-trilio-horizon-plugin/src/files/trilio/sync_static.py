4a0a892c1e108e819a0f7bd664a1d5bf3b911e35
sachin-trilio <sachin-trilio@trilio.io>
2020-02-13 08:03:14 -0500

    TVAULT-3520: Fixed indentation issue in horizon-plugin sync_static.py

--- juju-charms/charm-trilio-horizon-plugin/src/files/trilio/sync_static.py
+++ juju-charms/charm-trilio-horizon-plugin/src/files/trilio/sync_static.py
@@ -1,11 +1,15 @@
 import sys
 import settings
 import subprocess
+
 ls = settings.INSTALLED_APPS
 data = ""
+
 for app in ls:
     if app != 'dashboards':
         data += "-i "+str(app)+" "
+
 cmd = "{} /usr/share/openstack-dashboard/manage.py collectstatic"\
       " --noinput {}".format(sys.executable, data)
+
 subprocess.call(cmd, shell=True)
