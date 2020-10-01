34b3b6ec2ee7a6d2c537bd6e24d1378469c08a29
shyam-biradar <shyam.biradar@trilio.io>
2020-10-01 15:17:37 +0530

    Added osp13 fixes

--- redhat-director-scripts/puppet5/trilio/manifests/tripleo/mysql.pp
+++ redhat-director-scripts/puppet5/trilio/manifests/tripleo/mysql.pp
@@ -7,15 +7,9 @@
 # [*bootstrap_node*]
 #   (Optional) The hostname of the node responsible for bootstrapping tasks
 #   Defaults to hiera('mysql_short_bootstrap_node_name')
-# [*step*]
-#   (Optional) The current step in deployment. See tripleo-heat-templates
-#   for more details.
-#   Defaults to hiera('step')
-#
-#
+
 class trilio::tripleo::mysql (
   $bootstrap_node                = hiera('mysql_short_bootstrap_node_name', undef),
-  $step                          = Integer(hiera('step')),
 ) {
 
   if $::hostname == downcase($bootstrap_node) {
@@ -24,7 +18,7 @@ class trilio::tripleo::mysql (
     $create_db = false
   }
 
-  if $step >= 2 and $create_db {
+  if $create_db {
     if hiera('trilio_datamover_api_enabled', false) {
       ::tripleo::profile::base::database::mysql::include_and_check_auth{'::trilio::db::mysql':}
     }    
