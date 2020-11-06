e887e564b924103e95ee0300575fb9eb020726d3
shyam-biradar <shyam.biradar@trilio.io>
2020-11-06 15:31:10 +0530

    Fixed RHOSP and Kolla conflicts

---
+++ redhat-director-scripts/rhosp13/scripts/prepare_trilio_images_dockerhub.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ $# -lt 2 ];then
+   echo "Script takes exacyly 2 argument"
+   echo -e "./prepare_trilio_images_dockerhub.sh <undercloud_ip> <container_tag(queens)>"
+   exit 1
+fi
+
+undercloud_ip=$1
+tag=$2
+
+##Login to redhat container registry
+#echo -e "Enter Redhat container registry credentials"
+#docker login docker.io
+
+## Prepare openstack horizon with trilio container
+docker pull docker.io/trilio/trilio-horizon-plugin:${tag}
+docker tag docker.io/trilio/trilio-horizon-plugin:${tag} ${undercloud_ip}:8787/trilio/trilio-horizon-plugin:${tag}
+docker push ${undercloud_ip}:8787/trilio/trilio-horizon-plugin:${tag}
+
+## Prepare trilio datamover container
+docker pull docker.io/trilio/trilio-datamover:${tag}
+docker tag docker.io/trilio/trilio-datamover:${tag} ${undercloud_ip}:8787/trilio/trilio-datamover:${tag}
+docker push ${undercloud_ip}:8787/trilio/trilio-datamover:${tag}
+
+## Prepare trilio datamover api container
+docker pull docker.io/trilio/trilio-datamover-api:${tag}
+docker tag docker.io/trilio/trilio-datamover-api:${tag} ${undercloud_ip}:8787/trilio/trilio-datamover-api:${tag}
+docker push ${undercloud_ip}:8787/trilio/trilio-datamover-api:${tag}
+
+## Update image locations in env file
+dm_image_name="${undercloud_ip}:8787\/trilio\/trilio-datamover:${tag}"
+dmapi_image_name="${undercloud_ip}:8787\/trilio\/trilio-datamover-api:${tag}"
+
+<<<<<<< HEAD:redhat-director-scripts/prepare_trilio_images_dockerhub.sh
+sed  -i "s/.*DockerTrilioDatamoverImage.*/\   DockerTrilioDatamoverImage:\ ${dm_image_name}/g" trilio_env.yaml
+sed  -i "s/.*DockerTrilioDmApiImage.*/   DockerTrilioDmApiImage: ${dmapi_image_name}/g" trilio_env.yaml
+=======
+sed  -i "s/.*DockerTrilioDatamoverImage.*/\   DockerTrilioDatamoverImage:\ ${dm_image_name}/g" $SCRIPT_DIR/../environments/trilio_env.yaml
+sed  -i "s/.*DockerTrilioDmApiImage.*/   DockerTrilioDmApiImage: ${dmapi_image_name}/g" $SCRIPT_DIR/../environments/trilio_env.yaml
+sed  -i "s/.*DockerHorizonImage.*/   DockerHorizonImage: ${trilio_horizon_image}/g" $SCRIPT_DIR/../environments/trilio_env.yaml
+>>>>>>> upstream/4.1OSI:redhat-director-scripts/rhosp13/scripts/prepare_trilio_images_dockerhub.sh
---
+++ redhat-director-scripts/rhosp16.1/scripts/prepare_trilio_images.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -lt 2 ];then
+   echo "Script takes exacyly 2 argument"
+   echo -e "./prepare_trilio_images.sh <UNDERCLOUD_REGISTRY_HOSTNAME> <CONTAINER_TAG>"
+   exit 1
+fi
+
+undercloud_hostname=$1
+tag=$2
+
+
+current_dir=$(pwd)
+base_dir="$(dirname $0)"
+
+if [ $base_dir = '.' ]
+then
+base_dir="$current_dir"
+fi
+
+source /home/stack/stackrc
+
+##Login to redhat container registry
+echo -e "Enter Redhat container registry credentials (registry.redhat.io)"
+podman login registry.connect.redhat.com
+podman pull registry.connect.redhat.com/trilio/trilio-horizon-plugin:${tag}
+podman pull registry.connect.redhat.com/trilio/trilio-datamover:${tag}
+podman pull registry.connect.redhat.com/trilio/trilio-datamover-api:${tag}
+
+openstack tripleo container image push --local registry.connect.redhat.com/trilio/trilio-datamover:${tag}
+
+openstack tripleo container image push --local registry.connect.redhat.com/trilio/trilio-datamover-api:${tag}
+
+openstack tripleo container image push --local registry.connect.redhat.com/trilio/trilio-horizon-plugin:${tag}
+
+
+## Update image locations in env file
+trilio_dm_image="${undercloud_hostname}:8787\/trilio\/trilio-datamover:${tag}"
+trilio_dmapi_image="${undercloud_hostname}:8787\/trilio\/trilio-datamover-api:${tag}"
+trilio_horizon_image="${undercloud_hostname}:8787\/trilio\/trilio-horizon-plugin:${tag}"
+
+<<<<<<< HEAD:redhat-director-scripts/podman_prepare_images.sh
+sed  -i "s/.*DockerTrilioDatamoverImage.*/\   DockerTrilioDatamoverImage:\ ${trilio_dm_image}/g" trilio_env_osp16.yaml
+sed  -i "s/.*DockerTrilioDmApiImage.*/   DockerTrilioDmApiImage: ${trilio_dmapi_image}/g" trilio_env_osp16.yaml
+sed  -i "s/.*ContainerHorizonImage.*/   ContainerHorizonImage: ${trilio_horizon_image}/g" trilio_env_osp16.yaml
+=======
+sed  -i "s/.*DockerTrilioDatamoverImage.*/\   DockerTrilioDatamoverImage:\ ${trilio_dm_image}/g" $SCRIPT_DIR/../environments/trilio_env.yaml
+sed  -i "s/.*DockerTrilioDmApiImage.*/   DockerTrilioDmApiImage: ${trilio_dmapi_image}/g" $SCRIPT_DIR/../environments/trilio_env.yaml
+sed  -i "s/.*ContainerHorizonImage.*/   ContainerHorizonImage: ${trilio_horizon_image}/g" $SCRIPT_DIR/../environments/trilio_env.yaml
+>>>>>>> upstream/4.1OSI:redhat-director-scripts/rhosp16.1/scripts/prepare_trilio_images.sh
