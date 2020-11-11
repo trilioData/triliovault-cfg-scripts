cdd1c5cb9bcc678d8e3cd26b9e5bdaba1cf9d5f8
Shyam Biradar <shyam.biradar@trilio.io>
2020-11-11 16:42:02 +0530

    sync with 4.1OSI branch

--- redhat-director-scripts/rhosp16.1/puppet/trilio/manifests/contego.pp
+++ redhat-director-scripts/rhosp16.1/puppet/trilio/manifests/contego.pp
@@ -1,7 +1,7 @@
 class trilio::contego (
     $backup_target_type                   = 'nfs',
-    $nfs_shares				  = undef,
-    $nfs_options			  = 'nolock,soft,timeo=180,intr',
+    $nfs_shares				              = undef,
+    $nfs_options			              = 'nolock,soft,timeo=180,intr',
     $s3_type                              = 'amazon_s3',
     $s3_accesskey                         = undef,
     $s3_secretkey                         = undef,
@@ -54,10 +54,6 @@ class trilio::contego (
 
     }
 
-    #class {'trilio::contego::install': }
-    #class {'trilio::contego::postinstall': }
-    #class {'trilio::contego::cgroup': }
-    #class {'trilio::contego::service': }
     class {'trilio::contego::config': }
 
 }
