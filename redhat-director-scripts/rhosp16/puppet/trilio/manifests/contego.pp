6da01fc5d6420f8f3b9d3da2170a5b922a10df00
Shyam Biradar <shyam.biradar@trilio.io>
2021-01-05 18:05:42 +0530

    TVAULT4159 added vault_s3_auth_version parameter to contego conf

--- redhat-director-scripts/rhosp16/puppet/trilio/manifests/contego.pp
+++ redhat-director-scripts/rhosp16/puppet/trilio/manifests/contego.pp
@@ -9,6 +9,7 @@ class trilio::contego (
     $s3_bucket                            = undef,
     $s3_endpoint_url                      = undef,
     $s3_signature_version                 = 'default',
+    $s3_auth_version                      = 'DEFAULT',
     $s3_ssl_enabled                       = 'False',
     $s3_ssl_cert                          = undef,
     $database_connection                  = undef,
