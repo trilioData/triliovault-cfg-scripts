e98c5990f3c49fd5893d14a077c70b6dbf24ed5a
Shyam Biradar <shyam.biradar@trilio.io>
2021-04-08 16:02:21 +0530

    updated trilio puppet module url validity to 20 years

--- redhat-director-scripts/rhosp13/scripts/upload_puppet_module.sh
+++ redhat-director-scripts/rhosp13/scripts/upload_puppet_module.sh
@@ -8,4 +8,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 rm -rf $SCRIPT_DIR/trilio-puppet-module
 mkdir $SCRIPT_DIR/trilio-puppet-module
 cp -R $SCRIPT_DIR/../puppet/trilio $SCRIPT_DIR/trilio-puppet-module/
-upload-puppet-modules -d trilio-puppet-module
+upload-puppet-modules --seconds 630720000 -d trilio-puppet-module
