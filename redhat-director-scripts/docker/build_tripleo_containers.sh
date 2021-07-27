51adf5af985c5e4f3be39645f9aeb42ffcfe7483
pushkar-mahajan <pushkar.mahajan@trilio.io>
2021-07-27 12:04:59 +0530

    TVAULT-4577 delorean-component-tripleo.repo unable to reach

--- redhat-director-scripts/docker/build_tripleo_containers.sh
+++ redhat-director-scripts/docker/build_tripleo_containers.sh
@@ -49,6 +49,7 @@ do
 	echo -e "Creating trilio-datamover container for tripleo ${openstack_release} ${openstack_platform}"
 	cd $base_dir/${build_dir}/trilio-datamover/
 	mv Dockerfile_${openstack_distro}_${openstack_release}_${openstack_platform} Dockerfile
+	curl https://trunk.rdoproject.org/centos8/component/tripleo/current/delorean.repo > delorean-component-tripleo.repo
 	buildah bud --format docker -t trilio/${openstack_distro}-${openstack_release}-${openstack_platform}-trilio-datamover:${tvault_version}-${openstack_distro} .
 
 
@@ -56,6 +57,7 @@ do
 	echo -e "Creating trilio-datamover container-api for tripleo ${openstack_release} ${openstack_platform}"
 	cd $base_dir/${build_dir}/trilio-datamover-api/
 	mv Dockerfile_${openstack_distro}_${openstack_release}_${openstack_platform} Dockerfile
+	curl https://trunk.rdoproject.org/centos8/component/tripleo/current/delorean.repo > delorean-component-tripleo.repo
 	buildah bud --format docker -t trilio/${openstack_distro}-${openstack_release}-${openstack_platform}-trilio-datamover-api:${tvault_version}-${openstack_distro} .
 
 
@@ -63,6 +65,7 @@ do
 	echo -e "Creating trilio-horizon-plugin container for tripleo ${openstack_release} ${openstack_platform}"
 	cd $base_dir/${build_dir}/trilio-horizon-plugin/
 	mv Dockerfile_${openstack_distro}_${openstack_release}_${openstack_platform} Dockerfile
+	curl https://trunk.rdoproject.org/centos8/component/tripleo/current/delorean.repo > delorean-component-tripleo.repo
 	buildah bud --format docker -t trilio/${openstack_distro}-${openstack_release}-${openstack_platform}-trilio-horizon-plugin:${tvault_version}-${openstack_distro} .
 
 	# Clean the build_dir
