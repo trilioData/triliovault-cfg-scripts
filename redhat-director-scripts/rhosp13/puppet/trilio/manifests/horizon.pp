65e0352d7697d4341e27a397bfd7621a143d3ee3
shyam-biradar <shyam.biradar@trilio.io>
2021-12-08 06:40:34 -0500

    Fixes

--- redhat-director-scripts/rhosp13/puppet/trilio/manifests/horizon.pp
+++ redhat-director-scripts/rhosp13/puppet/trilio/manifests/horizon.pp
@@ -4,30 +4,30 @@ class trilio::horizon (
 
   tag 'triliohorizonconfig'
 
-    file { '$horizon_dir/local_settings.d':
+    file { "${horizon_dir}/local_settings.d":
         ensure => 'directory',
     }->
     file { "Create trilio dashboard file":
-        path   => '$horizon_dir/local_settings.d/_001_trilio_dashboard.py',
+        path   => "${horizon_dir}/local_settings.d/_001_trilio_dashboard.py",
         ensure => present,
     }->
     if $barbican_api_enabled  == 'true' {
         file_line { 'set variable OPENSTACK_ENCRYPTION_SUPPORT':
             ensure => present,
-            path   => '$horizon_dir/local_settings.d/_001_trilio_dashboard.py',
+            path   => "${horizon_dir}/local_settings.d/_001_trilio_dashboard.py",
             line   => 'OPENSTACK_ENCRYPTION_SUPPORT = True',
         }
     }
     else {
         file_line { 'set variable OPENSTACK_ENCRYPTION_SUPPORT':
             ensure => present,
-            path   => '$horizon_dir/local_settings.d/_001_trilio_dashboard.py',
+            path   => "${horizon_dir}/local_settings.d/_001_trilio_dashboard.py",
             line   => 'OPENSTACK_ENCRYPTION_SUPPORT = False',
         }
     }->
     file_line { 'set variable TRILIO_ENCRYPTION_SUPPORT':
         ensure => present,
-        path   => '$horizon_dir/local_settings.d/_001_trilio_dashboard.py',
+        path   => "${horizon_dir}/local_settings.d/_001_trilio_dashboard.py",
         line   => 'TRILIO_ENCRYPTION_SUPPORT = False',
     }
 
