80d8ec665b243bf66b02436d171cfc37e1eb64da
shyam-biradar <shyam.biradar@trilio.io>
2021-12-10 08:04:31 +0530

    fixes

--- redhat-director-scripts/rhosp13/puppet/trilio/manifests/horizon.pp
+++ redhat-director-scripts/rhosp13/puppet/trilio/manifests/horizon.pp
@@ -29,6 +29,13 @@ class trilio::horizon (
         ensure => present,
         path   => "${horizon_dir}/local_settings.d/_001_trilio_dashboard.py",
         line   => 'TRILIO_ENCRYPTION_SUPPORT = False',
+    }->
+    file_line { 'set variable TRILIO_ENCRYPTION_SUPPORT':
+        ensure => present,
+        path   => "${horizon_dir}/local_settings.d/_001_trilio_dashboard.py",
+        line   => "HORIZON_CONFIG['customization_module'] = 'dashboards.overrides'",
     }
+
+    
 
 }
