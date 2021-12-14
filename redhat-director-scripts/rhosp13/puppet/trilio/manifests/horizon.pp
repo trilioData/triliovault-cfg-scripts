7e76f15b7a23f22d47a5b166a02c0b54c7bdeb06
shyam-biradar <shyam.biradar@trilio.io>
2021-12-14 10:51:20 +0530

    Added fixes for tv4669 kolla ansible

--- redhat-director-scripts/rhosp13/puppet/trilio/manifests/horizon.pp
+++ redhat-director-scripts/rhosp13/puppet/trilio/manifests/horizon.pp
@@ -7,35 +7,9 @@ class trilio::horizon (
     file { "${horizon_dir}/local_settings.d":
         ensure => 'directory',
     }->
-    file { "Create trilio dashboard file":
-        path   => "${horizon_dir}/local_settings.d/_001_trilio_dashboard.py",
-        ensure => present,
-    }->
-    if $barbican_api_enabled  == 'true' {
-        file_line { 'set variable OPENSTACK_ENCRYPTION_SUPPORT':
-            ensure => present,
-            path   => "${horizon_dir}/local_settings.d/_001_trilio_dashboard.py",
-            line   => 'OPENSTACK_ENCRYPTION_SUPPORT = True',
-        }
-    }
-    else {
-        file_line { 'set variable OPENSTACK_ENCRYPTION_SUPPORT':
-            ensure => present,
-            path   => "${horizon_dir}/local_settings.d/_001_trilio_dashboard.py",
-            line   => 'OPENSTACK_ENCRYPTION_SUPPORT = False',
-        }
-    }->
-    file_line { 'set variable TRILIO_ENCRYPTION_SUPPORT':
-        ensure => present,
-        path   => "${horizon_dir}/local_settings.d/_001_trilio_dashboard.py",
-        line   => 'TRILIO_ENCRYPTION_SUPPORT = False',
-    }->
-    file_line { 'set variable TRILIO_ENCRYPTION_SUPPORT':
-        ensure => present,
-        path   => "${horizon_dir}/local_settings.d/_001_trilio_dashboard.py",
-        line   => "HORIZON_CONFIG['customization_module'] = 'dashboards.overrides'",
-    }
-
-    
+    file { "${horizon_dir}/local_settings.d/_002_trilio_dashboard.py":
+        ensure  => present,
+        content => template('trilio/_002_trilio_dashboard.py.erb'),
+    }   
 
 }
