ce0d693cfbfa4efc78ae3472212a290894309592
shyam-biradar <shyam.biradar@trilio.io>
2022-05-24 09:11:12 +0530

    Added triliovault helm chart for openstack helm distro

---
+++ openstack-helm/triliovault/utils/uninstall_triliovault_chart_mosk.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+helm delete triliovault
+kubectl delete job triliovault-datamover-db-init -n triliovault
+kubectl delete job triliovault-wlm-db-init -n triliovault
+kubectl delete job triliovault-datamover-db-sync -n triliovault
+kubectl delete job triliovault-wlm-db-sync -n triliovault
+kubectl delete job triliovault-datamover-ks-service -n triliovault
+kubectl delete job triliovault-datamover-ks-endpoints -n triliovault
+kubectl delete job triliovault-datamover-ks-user -n triliovault
+kubectl delete job triliovault-wlm-ks-endpoints -n triliovault
+kubectl delete job triliovault-wlm-ks-service -n triliovault
+kubectl delete job triliovault-wlm-ks-user -n triliovault
+kubectl delete job triliovault-wlm-rabbit-init -n triliovault
+kubectl delete job triliovault-datamover-db-drop
+kubectl delete job triliovault-wlm-db-drop
+sleep 50s
+
+kubectl get pods -n triliovault | grep trilio
+kubectl get jobs -n triliovault | grep trilio
+kubectl get pv -n triliovault | grep trilio
+
