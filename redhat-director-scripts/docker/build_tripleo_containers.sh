74e3f30932516f00e2bb1ce05788486a0112f890
Rajneesh <rajneesh.kapoor@trilio.io>
2022-06-02 07:53:43 -0400

    Removing wallaby & centos8 for 4.2.HF2

--- redhat-director-scripts/docker/build_tripleo_containers.sh
+++ redhat-director-scripts/docker/build_tripleo_containers.sh
@@ -26,7 +26,7 @@ declare -a openstack_releases=("train")
 
 declare -a openstack_platforms=("centos7")
 
-declare -a base_containers=("docker.io/tripleotrain/centos-binary-nova-compute:current-tripleo-rdo" "docker.io/tripleotrain/centos-binary-nova-api:current-tripleo-rdo" "docker.io/tripleotrain/centos-binary-horizon:current-tripleo-rdo" "docker.io/tripleotraincentos8/centos-binary-nova-compute:current-tripleo-rdo" "docker.io/tripleotraincentos8/centos-binary-nova-api:current-tripleo-rdo" "docker.io/tripleotraincentos8/centos-binary-horizon:current-tripleo-rdo")
+declare -a base_containers=("docker.io/tripleotrain/centos-binary-nova-compute:current-tripleo-rdo" "docker.io/tripleotrain/centos-binary-nova-api:current-tripleo-rdo" "docker.io/tripleotrain/centos-binary-horizon:current-tripleo-rdo")
 
 for base_container in "${base_containers[@]}"
 do
