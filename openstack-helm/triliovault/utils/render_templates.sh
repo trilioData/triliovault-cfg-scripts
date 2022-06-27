136f27bc3f3326cd663153fe0ee4c78573897cfc
shyam-biradar <shyam.biradar@trilio.io>
2022-06-27 12:02:25 +0000

    multi-ip fixes

--- openstack-helm/triliovault/utils/render_templates.sh
+++ openstack-helm/triliovault/utils/render_templates.sh
@@ -2,7 +2,8 @@
 cd ../..
 helm template -f triliovault/values_overrides/admin_creds.yaml \
 -f triliovault/values_overrides/image_pull_secrets.yaml \
--f triliovault/values_overrides/conf_triliovault.yaml \
+-f triliovault/values_overrides/cloud_admin.yaml \
+-f triliovault/values_overrides/s3.yaml \
 -f triliovault/values_overrides/ceph.yaml \
 -f triliovault/values_overrides/tls_public_endpoint.yaml \
 -f triliovault/values_overrides/ingress.yaml \
