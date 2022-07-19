3ea4ad5517489c92870726656128b96a043b1108
Rajneesh <rajneesh.kapoor@trilio.io>
2022-07-19 08:01:55 -0400

    Temp Change for 4.1.HF12 container creation

--- redhat-director-scripts/docker/publish_tripleo_containers.sh
+++ redhat-director-scripts/docker/publish_tripleo_containers.sh
@@ -25,13 +25,13 @@ do
     for openstack_platform in "${openstack_platforms[@]}"
     do
         container_prefix="${openstack_distro}-${openstack_release}-${openstack_platform}"
-        podman tag trilio/${container_prefix}-trilio-datamover-api:${tvault_version}-${openstack_distro} \
-        docker.io/trilio/${container_prefix}-trilio-datamover-api:${tvault_version}-${openstack_distro}
-        podman push --authfile /root/auth.json docker.io/trilio/${container_prefix}-trilio-datamover-api:${tvault_version}-${openstack_distro}
+        #podman tag trilio/${container_prefix}-trilio-datamover-api:${tvault_version}-${openstack_distro} \
+        #docker.io/trilio/${container_prefix}-trilio-datamover-api:${tvault_version}-${openstack_distro}
+        #podman push --authfile /root/auth.json docker.io/trilio/${container_prefix}-trilio-datamover-api:${tvault_version}-${openstack_distro}
 
-        podman tag trilio/${container_prefix}-trilio-datamover:${tvault_version}-${openstack_distro} \
-        docker.io/trilio/${container_prefix}-trilio-datamover:${tvault_version}-${openstack_distro}
-        podman push --authfile /root/auth.json docker.io/trilio/${container_prefix}-trilio-datamover:${tvault_version}-${openstack_distro}
+        #podman tag trilio/${container_prefix}-trilio-datamover:${tvault_version}-${openstack_distro} \
+        #docker.io/trilio/${container_prefix}-trilio-datamover:${tvault_version}-${openstack_distro}
+        #podman push --authfile /root/auth.json docker.io/trilio/${container_prefix}-trilio-datamover:${tvault_version}-${openstack_distro}
 
         podman tag trilio/${container_prefix}-trilio-horizon-plugin:${tvault_version}-${openstack_distro} \
         docker.io/trilio/${container_prefix}-trilio-horizon-plugin:${tvault_version}-${openstack_distro}
