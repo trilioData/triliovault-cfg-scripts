c15df729e8f2d8e48e2e9ea8f95d5b346ae1b938
shyam-biradar <shyam.biradar@trilio.io>
2022-08-22 15:03:48 +0530

    Fixes in upgrade.sh

--- openstack-helm/triliovault/utils/upgrade.sh
+++ openstack-helm/triliovault/utils/upgrade.sh
@@ -4,7 +4,8 @@ cd ../../
 
 helm upgrade triliovault ./triliovault --namespace=triliovault \
 --values=./triliovault/values_overrides/image_pull_secrets.yaml \
---values=./triliovault/values_overrides/conf_triliovault.yaml \
+--values=./triliovault/values_overrides/keystone.yaml \
+--values=./triliovault/values_overrides/nfs.yaml \
 --values=./triliovault/values_overrides/victoria-ubuntu_focal.yaml \
 --values=./triliovault/values_overrides/admin_creds.yaml \
 --values=./triliovault/values_overrides/tls_public_endpoint.yaml \
