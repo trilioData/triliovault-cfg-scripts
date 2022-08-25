67ccaff59dc33c4d9933e1f4d75ef0b92a3a8e82
shyam-biradar <shyam.biradar@trilio.io>
2022-08-25 18:16:41 +0530

    Removed deprecated, unsupported devops code

--- tripleo-scripts/puppet/trilio/manifests/tripleo/api.pp
+++
@@ -1,8 +0,0 @@
-class trilio::tripleo::api (
-  $step = hiera('step'),
-) {
-  if $step >= 5 {
-    # Trilio
-    include ::trilio::api
-  }
-}
--- tripleo-scripts/puppet/trilio/spec/classes/init_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-describe 'trilio' do
-
-  context 'with defaults for all parameters' do
-    it { should contain_class('trilio') }
-  end
-end
