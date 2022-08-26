049271fd2d558ba46f82a3891e8896b0a49241df
shyam-biradar <shyam.biradar@trilio.io>
2022-08-26 17:44:37 +0530

    naming convention fixes

--- redhat-director-scripts/rhosp16/puppet/trilio/manifests/tripleo/mysql_dmapi.pp
+++ redhat-director-scripts/rhosp16/puppet/trilio/manifests/tripleo/mysql_dmapi.pp
@@ -1,4 +1,4 @@
-# == Class: trilio::tripleo::mysql
+# == Class: trilio::tripleo::mysql_dmapi
 #
 # MySQL profile for tripleo
 #
@@ -8,7 +8,7 @@
 #   (Optional) The hostname of the node responsible for bootstrapping tasks
 #   Defaults to hiera('mysql_short_bootstrap_node_name')
 
-class trilio::tripleo::mysql (
+class trilio::tripleo::mysql_dmapi (
   $bootstrap_node                = hiera('mysql_short_bootstrap_node_name', undef),
 ) {
 
@@ -20,7 +20,7 @@ class trilio::tripleo::mysql (
 
   if $create_db {
     if hiera('trilio_datamover_api_enabled', false) {
-      tripleo::profile::base::database::mysql::include_and_check_auth{'::trilio::db::mysql':}
+      tripleo::profile::base::database::mysql::include_and_check_auth{'::trilio::dmapi::db::mysql':}
     }    
   }
 }
