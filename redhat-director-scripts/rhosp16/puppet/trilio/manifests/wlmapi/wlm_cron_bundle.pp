3ba672936d5aa5c051e93820e70d2cfa0aaaa13a
shyam-biradar <shyam.biradar@trilio.io>
2022-10-14 14:41:59 +0530

    pacemaker fixes

--- redhat-director-scripts/rhosp16/puppet/trilio/manifests/wlmapi/wlm_cron_bundle.pp
+++ redhat-director-scripts/rhosp16/puppet/trilio/manifests/wlmapi/wlm_cron_bundle.pp
@@ -65,16 +65,16 @@
 #   (optional) Set the --user= switch to be passed to pcmk
 #   Defaults to 'root'
 #
-class trilio::wlmapi::pacemaker::wlm_cron_bundle (
-  $bootstrap_node             = hiera('cinder_volume_short_bootstrap_node_name'),
-  $cinder_volume_docker_image = hiera('tripleo::profile::pacemaker::cinder::volume_bundle::cinder_volume_docker_image', undef),
+class trilio::wlmapi::wlm_cron_bundle (
+  $bootstrap_node             = hiera('triliovault_wlm_cron_short_bootstrap_node_name'),
+  $triliovault_wlm_cron_docker_image = hiera('trilio::wlmapi::wlm_cron.bundle::triliovault_wlm_cron_docker_image', undef),
   $docker_volumes             = [],
   $docker_environment         = {'KOLLA_CONFIG_STRATEGY' => 'COPY_ALWAYS'},
   $pcs_tries                  = hiera('pcs_tries', 20),
   $step                       = Integer(hiera('step')),
   $container_backend          = 'docker',
   $log_driver                 = undef,
-  $log_file                   = '/var/log/containers/stdouts/openstack-cinder-volume.log',
+  $log_file                   = '/var/log/containers/triliovault-wlm-cron/triliovault-wlm-cron.log',
   $tls_priorities             = hiera('tripleo::pacemaker::tls_priorities', undef),
   $bundle_user                = 'root',
 ) {
@@ -98,10 +98,9 @@ class trilio::wlmapi::pacemaker::wlm_cron_bundle (
   } else {
     $log_file_real = ''
   }
-  include ::tripleo::profile::base::cinder::volume
 
   if $step >= 2 and $pacemaker_master {
-    $cinder_volume_short_node_names = hiera('cinder_volume_short_node_names')
+    $triliovault_wlm_cron_short_node_names = hiera('triliovault_wlm_cron_short_node_names')
 
     if (hiera('pacemaker_short_node_names_override', undef)) {
       $pacemaker_short_node_names = hiera('pacemaker_short_node_names_override')
@@ -109,14 +108,14 @@ class trilio::wlmapi::pacemaker::wlm_cron_bundle (
       $pacemaker_short_node_names = hiera('pacemaker_short_node_names')
     }
 
-    $pcmk_cinder_volume_nodes = intersection($cinder_volume_short_node_names, $pacemaker_short_node_names)
-    $pcmk_cinder_volume_nodes.each |String $node_name| {
-      pacemaker::property { "cinder-volume-role-${node_name}":
-        property => 'cinder-volume-role',
+    $pcmk_triliovault_wlm_cron_nodes = intersection($triliovault_wlm_cron_short_node_names, $pacemaker_short_node_names)
+    $pcmk_triliovault_wlm_cron_nodes.each |String $node_name| {
+      pacemaker::property { "triliovault-wlm-cron-role-${node_name}":
+        property => 'triliovault-wlm-cron-role',
         value    => true,
         tries    => $pcs_tries,
         node     => downcase($node_name),
-        before   => Pacemaker::Resource::Bundle[$::cinder::params::volume_service],
+        before   => Pacemaker::Resource::Bundle['triliovault-wlm-cron'],
       }
     }
   }
@@ -126,90 +125,55 @@ class trilio::wlmapi::pacemaker::wlm_cron_bundle (
       $docker_vol_arr = delete(any2array($docker_volumes), '').flatten()
 
       unless empty($docker_vol_arr) {
-        $storage_maps = docker_volumes_to_storage_maps($docker_vol_arr, 'cinder-volume')
+        $storage_maps = docker_volumes_to_storage_maps($docker_vol_arr, 'triliovault-wlm-cron')
       } else {
-        notice('Using fixed list of docker volumes for cinder-volume bundle')
+        notice('Using fixed list of docker volumes for triliovault_wlm_cron bundle')
         # Default to previous hard-coded list
         $storage_maps = {
-          'cinder-volume-cfg-files'               => {
-            'source-dir' => '/var/lib/kolla/config_files/cinder_volume.json',
+          'triliovault-wlm-cron-cfg-files'               => {
+            'source-dir' => '/var/lib/kolla/config_files/triliovault_wlm_cron.json',
             'target-dir' => '/var/lib/kolla/config_files/config.json',
             'options'    => 'ro',
           },
-          'cinder-volume-cfg-data'                => {
-            'source-dir' => '/var/lib/config-data/puppet-generated/cinder/',
-            'target-dir' => '/var/lib/kolla/config_files/src',
+          'triliovault-wlm-cron-cfg-data'                => {
+            'source-dir' => '/var/lib/config-data/puppet-generated/triliovaultwlmcron/',
+            'target-dir' => '/var/lib/kolla/config_files/triliovaultwlmcron',
             'options'    => 'ro',
           },
-          'cinder-volume-hosts'                   => {
+          'triliovault-wlm-cron-hosts'                   => {
             'source-dir' => '/etc/hosts',
             'target-dir' => '/etc/hosts',
             'options'    => 'ro',
           },
-          'cinder-volume-localtime'               => {
+          'triliovault-wlm-cron-localtime'               => {
             'source-dir' => '/etc/localtime',
             'target-dir' => '/etc/localtime',
             'options'    => 'ro',
           },
-          'cinder-volume-dev'                     => {
-            'source-dir' => '/dev',
-            'target-dir' => '/dev',
-            'options'    => 'rw',
-          },
-          'cinder-volume-run'                     => {
-            'source-dir' => '/run',
-            'target-dir' => '/run',
-            'options'    => 'rw',
-          },
-          'cinder-volume-sys'                     => {
-            'source-dir' => '/sys',
-            'target-dir' => '/sys',
-            'options'    => 'rw',
-          },
-          'cinder-volume-lib-modules'             => {
-            'source-dir' => '/lib/modules',
-            'target-dir' => '/lib/modules',
-            'options'    => 'ro',
-          },
-          'cinder-volume-iscsi'                   => {
-            'source-dir' => '/etc/iscsi',
-            'target-dir' => '/var/lib/kolla/config_files/src-iscsid',
-            'options'    => 'ro',
-          },
-          'cinder-volume-var-lib-cinder'          => {
-            'source-dir' => '/var/lib/cinder',
-            'target-dir' => '/var/lib/cinder',
-            'options'    => 'rw',
-          },
-          'cinder-volume-pki-extracted'           => {
+          'triliovault-wlm-cron-pki-extracted'           => {
             'source-dir' => '/etc/pki/ca-trust/extracted',
             'target-dir' => '/etc/pki/ca-trust/extracted',
             'options'    => 'ro',
           },
-          'cinder-volume-pki-ca-bundle-crt'       => {
+          'triliovault-wlm-cron-pki-ca-bundle-crt'       => {
             'source-dir' => '/etc/pki/tls/certs/ca-bundle.crt',
             'target-dir' => '/etc/pki/tls/certs/ca-bundle.crt',
             'options'    => 'ro',
           },
-          'cinder-volume-pki-ca-bundle-trust-crt' => {
+          'triliovault-wlm-cron-pki-ca-bundle-trust-crt' => {
             'source-dir' => '/etc/pki/tls/certs/ca-bundle.trust.crt',
             'target-dir' => '/etc/pki/tls/certs/ca-bundle.trust.crt',
             'options'    => 'ro',
           },
-          'cinder-volume-pki-cert'                => {
+          'triliovault-wlm-cron-pki-cert'                => {
             'source-dir' => '/etc/pki/tls/cert.pem',
             'target-dir' => '/etc/pki/tls/cert.pem',
             'options'    => 'ro',
           },
-          'cinder-volume-var-log'                 => {
-            'source-dir' => '/var/log/containers/cinder',
-            'target-dir' => '/var/log/cinder',
+          'triliovault-wlm-cron-var-log'                 => {
+            'source-dir' => '/var/log/containers/triliovault-wlm-cron',
+            'target-dir' => '/var/log/triliovault',
             'options'    => 'rw',
-          },
-          'cinder-volume-ceph-cfg-dir'            => {
-            'source-dir' => '/etc/ceph',
-            'target-dir' => '/var/lib/kolla/config_files/src-ceph/',
-            'options'    => 'ro',
           },
         }
       }
@@ -227,13 +191,13 @@ class trilio::wlmapi::pacemaker::wlm_cron_bundle (
         $tls_priorities_real = ''
       }
 
-      pacemaker::resource::bundle { $::cinder::params::volume_service:
-        image             => $cinder_volume_docker_image,
+      pacemaker::resource::bundle { 'triliovault-wlm-cron':
+        image             => $triliovault_wlm_cron_docker_image,
         replicas          => 1,
         location_rule     => {
           resource_discovery => 'exclusive',
           score              => 0,
-          expression         => ['cinder-volume-role eq true'],
+          expression         => ['triliovault-wlm-cron-role eq true'],
         },
         container_options => 'network=host',
         # lint:ignore:140chars
