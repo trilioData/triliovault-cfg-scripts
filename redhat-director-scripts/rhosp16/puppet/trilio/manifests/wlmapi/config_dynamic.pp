77e084b42c5f7675f6b9f4b399e4e8b052315bba
shyam-biradar <shyam.biradar@trilio.io>
2023-02-13 17:41:36 +0530

    Revert "Added code to set cloud_unique_id"
    This reverts commit 05b4f0d23c8c0e98f3fcd9d3cf5142e10b782da4.

--- redhat-director-scripts/rhosp16/puppet/trilio/manifests/wlmapi/config_dynamic.pp
+++
@@ -1,8 +0,0 @@
-class trilio::wlmapi::config_dynamic {
-    tag 'wlmapiconfigdynamic'
-      exec{ "get keystone resources":
-          command => '/etc/triliovault-wlm/get_keystone_resources.sh',
-          provider => shell,
-      }
-
-}
