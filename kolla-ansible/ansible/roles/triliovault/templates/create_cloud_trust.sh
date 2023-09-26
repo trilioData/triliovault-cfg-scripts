7957a86073552756fe09545250b9cb960ac8149c
shyam-biradar <shyam.biradar@trilio.io>
2023-09-26 13:14:26 +0530

    Added retries for wlm cloud trust creation command, sometime wlm services takes time to get up, that's why this is needed

--- kolla-ansible/ansible/roles/triliovault/templates/create_cloud_trust.sh
+++ kolla-ansible/ansible/roles/triliovault/templates/create_cloud_trust.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-set -ex
+set -x
 ##Ensure cloudrc is present
 cloudrc=/opt/triliovault-cloudrc
 
 if [[ -f "$cloudrc" ]]; then
-	printf "Cloudrc found at $cloudrc \n\n Run command to create cloud admin trust\n"
-	. $cloudrc
-	workloadmgr trust-create --is_cloud_trust True admin --insecure
+        printf "Cloudrc found at $cloudrc \n\n Run command to create cloud admin trust\n"
+        . $cloudrc
+        for attempt in {1..5};
+        do
+                echo -e "Attempting to create wlm-cloud admin trust, Attempt Number: $attempt"
+                workloadmgr trust-create --is_cloud_trust True admin --insecure
+                status=$?
+                if [ $status -eq 0 ]; then
+                        echo -e "wlm cloud admin trust created successfully"
+                        break
+                else
+                        echo -e "wlm cloud admin trust create command failed. Will re-try after 30 seconds"
+                        if [ $attempt -eq 5 ]; then
+                           echo -e "Five attempts done, but wlm cloud trust creation still failing. Exiting."
+                           exit $status
+                        fi
+                        sleep 30s
+                        continue
+                fi
+        done
 else
-	printf "Failed to create cloud admin trust, cause cloudrc not found at $cloudrc"
+        printf "Failed to create cloud admin trust, cause cloudrc not found at $cloudrc"
 fi
