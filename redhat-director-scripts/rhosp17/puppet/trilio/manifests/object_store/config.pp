42a36f16ccb5f9c8fd268640edf9c7aeef67a2b5
shyam-biradar <shyam.biradar@trilio.io>
2024-05-07 10:41:12 +0000

    Added fixes for multi backup target feature

--- redhat-director-scripts/rhosp17/puppet/trilio/manifests/object_store/config.pp
+++ redhat-director-scripts/rhosp17/puppet/trilio/manifests/object_store/config.pp
@@ -18,14 +18,35 @@ class trilio::object_store::config inherits trilio::object_store {
     $backup_targets.each |$target| {
       # Check if backup_target_type = s3
       if $target['backup_target_type'] == 's3' {
+        if $target['s3_type'] == 'amazon_s3' {
+          $backup_target_mount_point = base64('encode', $target['s3_bucket'])
+        }
+        else {
+          $s3_domain_name = regsubst($target['s3_endpoint_url'], '^https?://', '')
+          $bucket_name = $target['s3_bucket']
+          $ceph_s3_str = "$s3_domain_name/$bucket_name"
+          $backup_target_mount_point = base64('encode', $ceph_s3_str)
+        }
+        file { "/etc/triliovault-object-store/s3-cert-${target['backup_target_name']}.pem":
+          ensure => 'present',
+          owner  => '42436',
+          group  => '42436',
+          mode   => '0644',
+          source => "puppet:///modules/trilio/s3-cert-${target['backup_target_name']}.pem",
+        }
+
         $conf_file_name = "/etc/triliovault-object-store/triliovault-object-store_${target['backup_target_name']}.conf"
 
         # Create conf file
         file { $conf_file_name:
           ensure  => present,
+          owner  => '42436',
+          group  => '42436',
+          mode   => '0644',
           content => epp('trilio/triliovault_object_store.conf.epp', {
             backup_target_type    => $target['backup_target_type'],
             backup_target_name    => $target['backup_target_name'],
+            backup_target_mount_point => $backup_target_mount_point,
             s3_accesskey          => $target['s3_access_key'],
             s3_secretkey          => $target['s3_secret_key'],
             s3_bucket             => $target['s3_bucket'],
@@ -40,10 +61,10 @@ class trilio::object_store::config inherits trilio::object_store {
           }),
         }
       }
+
+
     }
 
 
-
-
-
 }
+
