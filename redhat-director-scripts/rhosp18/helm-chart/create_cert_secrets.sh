c78ba48f67306a15bff999924ad0d30ba0d504c9
shyam-biradar <shyam.biradar@trilio.io>
2024-11-22 07:27:49 -0500

    Added fixes to create secret script

--- redhat-director-scripts/rhosp18/helm-chart/create_cert_secrets.sh
+++ redhat-director-scripts/rhosp18/helm-chart/create_cert_secrets.sh
@@ -5,6 +5,9 @@ oc create -f ./certificate.yaml
 oc get secret cert-triliovault-wlm-public-svc -n openstack -o yaml > cert-triliovault-wlm-public-svc.yaml
 oc get secret cert-triliovault-wlm-internal-svc -n openstack -o yaml > cert-triliovault-wlm-internal-svc.yaml
 
+sed -i 's/openstack/triliovault/' cert-triliovault-wlm-internal-svc.yaml
+sed -i 's/openstack/triliovault/' cert-triliovault-wlm-public-svc.yaml
+
 oc create -f cert-triliovault-wlm-public-svc.yaml
 oc create -f cert-triliovault-wlm-internal-svc.yaml
 
@@ -14,6 +17,9 @@ oc describe secret cert-triliovault-wlm-internal-svc
 
 oc get secret cert-triliovault-datamover-public-svc -n openstack -o yaml > cert-triliovault-datamover-public-svc.yaml
 oc get secret cert-triliovault-datamover-internal-svc -n openstack -o yaml > cert-triliovault-datamover-internal-svc.yaml
+
+sed -i 's/openstack/triliovault/' cert-triliovault-datamover-internal-svc.yaml
+sed -i 's/openstack/triliovault/' cert-triliovault-datamover-public-svc.yaml
 
 oc create -f cert-triliovault-datamover-public-svc.yaml
 oc create -f cert-triliovault-datamover-internal-svc.yaml
