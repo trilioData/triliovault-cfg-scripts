fa1f66a754aa5090f2a80da22a2dcf915d24502c
shyam-biradar <shyam.biradar@trilio.io>
2024-12-09 11:32:33 +0530

    Removed unnecessary code

---
+++ redhat-director-scripts/rhosp18/utils/create_cert_secrets.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+oc create -f ./certificate.yaml
+
+oc get secret cert-triliovault-wlm-public-svc -n openstack -o yaml > cert-triliovault-wlm-public-svc.yaml
+oc get secret cert-triliovault-wlm-internal-svc -n openstack -o yaml > cert-triliovault-wlm-internal-svc.yaml
+
+sed -i 's/openstack/triliovault/' cert-triliovault-wlm-internal-svc.yaml
+sed -i 's/openstack/triliovault/' cert-triliovault-wlm-public-svc.yaml
+
+oc create -f cert-triliovault-wlm-public-svc.yaml
+oc create -f cert-triliovault-wlm-internal-svc.yaml
+
+oc describe secret cert-triliovault-wlm-public-svc
+oc describe secret cert-triliovault-wlm-internal-svc
+
+
+oc get secret cert-triliovault-datamover-public-svc -n openstack -o yaml > cert-triliovault-datamover-public-svc.yaml
+oc get secret cert-triliovault-datamover-internal-svc -n openstack -o yaml > cert-triliovault-datamover-internal-svc.yaml
+
+sed -i 's/openstack/triliovault/' cert-triliovault-datamover-internal-svc.yaml
+sed -i 's/openstack/triliovault/' cert-triliovault-datamover-public-svc.yaml
+
+oc create -f cert-triliovault-datamover-public-svc.yaml
+oc create -f cert-triliovault-datamover-internal-svc.yaml
+
+oc describe secret cert-triliovault-datamover-public-svc
+oc describe secret cert-triliovault-datamover-internal-svc
+#!/bin/bash
+
+# Define variables
+SOURCE_NAMESPACE="openstack"
+TARGET_NAMESPACE="triliovault"
+SECRET_NAME="combined-ca-bundle"
+
+# Check if the secret exists in the source namespace
+if ! kubectl get secret "$SECRET_NAME" -n "$SOURCE_NAMESPACE" > /dev/null 2>&1; then
+  echo "Error: Secret '$SECRET_NAME' does not exist in namespace '$SOURCE_NAMESPACE'"
+  exit 1
+fi
+
+# Extract and decode specific data items from the secret
+INTERNAL_CA_BUNDLE=$(kubectl get secret "$SECRET_NAME" -n "$SOURCE_NAMESPACE" -o jsonpath='{.data.internal-ca-bundle\.pem}' | base64 -d)
+TLS_CA_BUNDLE=$(kubectl get secret "$SECRET_NAME" -n "$SOURCE_NAMESPACE" -o jsonpath='{.data.tls-ca-bundle\.pem}' | base64 -d)
+
+# Check if data items were retrieved successfully
+if [[ -z "$INTERNAL_CA_BUNDLE" || -z "$TLS_CA_BUNDLE" ]]; then
+  echo "Error: Failed to fetch and decode required data from the secret '$SECRET_NAME'."
+  exit 1
+fi
+
+# Create a temporary directory for the decoded files
+TEMP_DIR=$(mktemp -d)
+echo "$INTERNAL_CA_BUNDLE" > "$TEMP_DIR/internal-ca-bundle.pem"
+echo "$TLS_CA_BUNDLE" > "$TEMP_DIR/tls-ca-bundle.pem"
+
+# Delete any existing secret in the target namespace
+kubectl delete secret "$SECRET_NAME" -n "$TARGET_NAMESPACE" 2>/dev/null || true
+
+# Create a new secret in the target namespace using the decoded files
+kubectl create secret generic "$SECRET_NAME" -n "$TARGET_NAMESPACE" \
+  --from-file=internal-ca-bundle.pem="$TEMP_DIR/internal-ca-bundle.pem" \
+  --from-file=tls-ca-bundle.pem="$TEMP_DIR/tls-ca-bundle.pem"
+
+# Clean up the temporary directory
+rm -rf "$TEMP_DIR"
+
+if [ $? -eq 0 ]; then
+  echo "Secret '$SECRET_NAME' has been successfully created in namespace '$TARGET_NAMESPACE'."
+else
+  echo "Error: Failed to create the secret in namespace '$TARGET_NAMESPACE'."
+  exit 1
+fi
+
--- redhat-director-scripts/rhosp18/utils/generate_dynamic_inputs.sh
+++ redhat-director-scripts/rhosp18/utils/generate_dynamic_inputs.sh
@@ -1,101 +1,58 @@
 #!/bin/bash -x
 set -ex
-
-if [ -z "$1" ]; then
-  echo "Usage: $0 <keystone_interface>"
-  exit 1
-fi
-
-KEYSTONE_INTERFACE=$1
-
-# Retrieve the Keystone URL
-KEYSTONE_URL=$(openstack endpoint list --interface $KEYSTONE_INTERFACE --service keystone -c URL -f value)
-
-if [[ -z "$KEYSTONE_URL" ]]; then
-  echo "Failed to retrieve Keystone URL"
-  exit 1
-fi
-
-# Extract protocol, host, and port using regex
-if [[ $KEYSTONE_URL =~ ^(https?)://([^:/]+)(:([0-9]+))?(/.*)?$ ]]; then
-  AUTH_PROTOCOL=${BASH_REMATCH[1]}
-  AUTH_HOST=${BASH_REMATCH[2]}
-  AUTH_PORT=${BASH_REMATCH[4]}
-else
-  echo "Invalid Keystone URL"
-fi
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-
-# Retrieve details of internal endpoint
-KEYSTONE_URL_INTERNAL=$(openstack endpoint list --interface internal --service keystone -c URL -f value)
-# Extract protocol, host, and port using regex
-if [[ $KEYSTONE_URL_INTERNAL =~ ^(https?)://([^:/]+)(:([0-9]+))?(/.*)?$ ]]; then
-  AUTH_PROTOCOL_INTERNAL=${BASH_REMATCH[1]}
-  AUTH_HOST_INTERNAL=${BASH_REMATCH[2]}
-else
-  echo "Invalid Keystone URL"
-fi
-
-# Retrieve details of internal endpoint
-KEYSTONE_URL_PUBLIC=$(openstack endpoint list --interface public --service keystone -c URL -f value)
-# Extract protocol, host, and port using regex
-if [[ $KEYSTONE_URL_PUBLIC =~ ^(https?)://([^:/]+)(:([0-9]+))?(/.*)?$ ]]; then
-  AUTH_PROTOCOL_PUBLIC=${BASH_REMATCH[1]}
-  AUTH_HOST_PUBLIC=${BASH_REMATCH[2]}
-else
-  echo "Invalid Keystone URL"
-fi
-# Retrieve details of internal endpoint
-KEYSTONE_URL_ADMIN=$(openstack endpoint list --interface admin --service keystone -c URL -f value)
-# Extract protocol, host, and port using regex
-if [[ $KEYSTONE_URL_ADMIN =~ ^(https?)://([^:/]+)(:([0-9]+))?(/.*)?$ ]]; then
-  AUTH_PROTOCOL_ADMIN=${BASH_REMATCH[1]}
-  AUTH_HOST_ADMIN=${BASH_REMATCH[2]}
-else
-  echo "Invalid Keystone URL"
-fi
-
-AUTH_URI="${KEYSTONE_URL%/}/v3"
 
 DMAPI_RABBIT_PASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${11:-42} | head -n 1`
 WLM_RABBIT_PASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${11:-42} | head -n 1`
 DMAPI_DB_PASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${11:-42} | head -n 1`
 WLM_DB_PASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${11:-42} | head -n 1`
-DMAPI_KEYSTONE_PASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${11:-42} | head -n 1`
-WLM_KEYSTONE_PASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${11:-42} | head -n 1`
+
+RABBIT_HOST=`oc -n openstack get secret rabbitmq-default-user -o jsonpath='{.data.host}' | base64 --decode`
+RABBIT_PORT=`oc -n openstack get secret rabbitmq-default-user -o jsonpath='{.data.port}' | base64 --decode`
+RABBIT_DRIVER=$(oc -n openstack get secret nova-api-config-data -o jsonpath='{.data.01-nova\.conf}' | base64 --decode | grep "^driver" | awk -F"=" '{print $2}' | xargs)
+DB_ROOT_PASSWORD=`oc -n openstack get secret osp-secret -o jsonpath='{.data.DbRootPassword}' | base64 --decode`
+DB_HOST=`oc -n openstack get secret nova-api-config-data -o jsonpath='{.data.01-nova\.conf}' | base64 --decode | awk '/connection =/ {match($0, /@([^?\/]+)/, arr); print arr[1]; exit}'`
+DB_PORT="3306"
+RABBIT_ADMIN_PASSWORD=""
 
 
+MEMCACHED_SERVERS=`oc -n openstack get memcached -o jsonpath='{.items[*].status.serverList[*]}' | tr ' ' ','`
+KEYSTONE_CA_CERT=`oc -n openstack get secret rootca-internal -o jsonpath='{.data.ca\.crt}' | base64 --decode`
+TRANSPORT_URL=`oc -n openstack get secret rabbitmq-transport-url-cinder-cinder-transport -o jsonpath='{.data.transport_url}' | base64 --decode`
 
 tee > ${SCRIPT_DIR}/trilio_inputs_dynamic.yaml << EOF
-database:
-  datamover_api:
-    password: $DMAPI_DB_PASSWORD
-  wlm_api:
-    password: $WLM_DB_PASSWORD
-rabbitmq:
-  datamover_api:
-    password: $DMAPI_RABBIT_PASSWORD
-  wlm_api:
-    password: $WLM_RABBIT_PASSWORD
-keystone:
-  datamover_api:
-    password: $DMAPI_KEYSTONE_PASSWORD
-  wlm_api:
-    password: $WLM_KEYSTONE_PASSWORD
+apiVersion: tvo.trilio.io/v1
+kind: TVOControlPlane
+spec:
   common:
-    auth_url: "$KEYSTONE_URL"
-    auth_uri: "$AUTH_URI"
-    keystone_auth_protocol: "$AUTH_PROTOCOL"
-    keystone_auth_host: "$AUTH_HOST"
-    keystone_auth_port: "$AUTH_PORT"
-  datamover_api:
-    internal_endpoint: "${AUTH_PROTOCOL_INTERNAL}://${AUTH_HOST_INTERNAL}:8784/v2"
-    public_endpoint: "${AUTH_PROTOCOL_PUBLIC}://${AUTH_HOST_PUBLIC}:8784/v2"
-    admin_endpoint: "${AUTH_PROTOCOL_ADMIN}://${AUTH_HOST_ADMIN}:8784/v2"
-  wlm_api:
-    internal_endpoint: "${AUTH_PROTOCOL_INTERNAL}://${AUTH_HOST_INTERNAL}:8781/v1/\$(tenant_id)s"
-    public_endpoint: "${AUTH_PROTOCOL_PUBLIC}://${AUTH_HOST_PUBLIC}:8781/v1/\$(tenant_id)s"
-    admin_endpoint: "${AUTH_PROTOCOL_ADMIN}://${AUTH_HOST_ADMIN}:8781/v1/\$(tenant_id)s"
+    memcached_servers: $MEMCACHED_SERVERS
+  database:
+    common:
+      root_user_name: "root"
+      root_password: $DB_ROOT_PASSWORD
+      host: $DB_HOST
+      port: $DB_PORT
+    datamover_api:
+      password: $DMAPI_DB_PASSWORD
+    wlm_api:
+      password: $WLM_DB_PASSWORD
+  rabbitmq:
+    common: 
+      admin_user: "rabbitmq"
+      admin_password: $RABBIT_ADMIN_PASSWORD
+      host: $RABBIT_HOST
+      port: $RABBIT_PORT
+      driver: $RABBIT_DRIVER
+    datamover_api:
+      transport_url: $TRANSPORT_URL
+      password: $DMAPI_RABBIT_PASSWORD
+    wlm_api:
+      transport_url: $TRANSPORT_URL
+      password: $WLM_RABBIT_PASSWORD
+  keystone:
+    common:
+      ca_cert: |
+  $(echo "$KEYSTONE_CA_CERT" | sed 's/^/      /')
 EOF
+
+echo -e "Dynamic values are generated and copied to file ${SCRIPT_DIR}/tvo-chart/values_overrides/trilio_inputs_dynamic.yaml"
