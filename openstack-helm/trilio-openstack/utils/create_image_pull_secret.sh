59c08b78ef5823343a6498b955c8d1ca76805d18
Shruti Anekar <97589399+shruti-trilio@users.noreply.github.com>
2025-04-27 17:56:49 +0530

    TVAULT-6685: Add --immutable parameter for buckets with object lock enabled (#1145)
    Add --immutable parameter for buckets with object lock enabled

--- openstack-helm/trilio-openstack/utils/create_image_pull_secret.sh
+++ openstack-helm/trilio-openstack/utils/create_image_pull_secret.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [ $# -lt 2 ];then
-   echo "Script takes exacyly 2 arguments"
+if [ $# -lt 2 ]; then
+   echo "Script takes exactly 2 arguments"
    echo -e "./get_admin_creds.sh <TRILIO_REGISTRY_USERNAME> <TRILIO_REGISTRY_PASSWORD>"
    exit 1
 fi
@@ -11,12 +11,17 @@ fi
 TRILIO_REGISTRY_USERNAME=$1
 TRILIO_REGISTRY_PASSWORD=$2
 
-kubectl create secret docker-registry triliovault-image-registry \
-   --docker-server="docker.io" \
-   --docker-username=${TRILIO_REGISTRY_USERNAME} \
-   --docker-password=${TRILIO_REGISTRY_PASSWORD} \
-   -n trilio-openstack
+# Namespaces to apply the secret in
+NAMESPACES=("trilio-openstack" "openstack")
 
+for NS in "${NAMESPACES[@]}"; do
+  echo "Creating secret in namespace: $NS"
+  kubectl create secret docker-registry triliovault-image-registry \
+     --docker-server="docker.io" \
+     --docker-username="${TRILIO_REGISTRY_USERNAME}" \
+     --docker-password="${TRILIO_REGISTRY_PASSWORD}" \
+     -n "$NS" --dry-run=client -o yaml | kubectl apply -f -
+done
 kubectl describe secret triliovault-image-registry -n trilio-openstack
 
-echo "Trilio image pull secret created. Name: triliovault-image-registry"
+echo "Trilio image pull secret created in both 'trilio-openstack' and 'openstack' namespaces."
