b0c64201d3c9c7b6fd25169b7558b78254978711
shyam-biradar <shyam.biradar@trilio.io>
2025-07-08 06:28:59 -0400

    Added fixes to script

--- redhat-director-scripts/rhosp18/ctlplane-scripts/renew_certs.sh
+++ redhat-director-scripts/rhosp18/ctlplane-scripts/renew_certs.sh
@@ -1,52 +1,56 @@
 #!/bin/bash
 
-# Get a single consistent timestamp
-RENEW_AT=$(date -Iseconds)
+set -e
 
-# Apply updated cert spec
-oc -n openstack apply -f ./certificate.yaml
+echo -e "Applying certificate.yaml"
 
-# Force renew all certs using the same timestamp
-oc annotate certificate triliovault-wlm-public-svc \
-  -n openstack cert-manager.io/renew-request-at="$RENEW_AT" --overwrite
+APPLY_OUTPUT=$(oc -n openstack apply -f ./certificate.yaml)
+echo $APPLY_OUTPUT
+TOTAL_LINES=$(echo "$APPLY_OUTPUT" | wc -l)
+UNCHANGED_LINES=$(echo "$APPLY_OUTPUT" | grep -c 'unchanged')
 
-oc annotate certificate triliovault-wlm-internal-svc \
-  -n openstack cert-manager.io/renew-request-at="$RENEW_AT" --overwrite
+if [ "$TOTAL_LINES" -eq "$UNCHANGED_LINES" ]; then
+  echo -e "\nAll certificate resources are unchanged. Exiting."
+  exit 0
+else
+  echo "Detected changes in certificate resources. Proceeding..."
+fi
 
-oc annotate certificate triliovault-datamover-public-svc \
-  -n openstack cert-manager.io/renew-request-at="$RENEW_AT" --overwrite
-
-oc annotate certificate triliovault-datamover-internal-svc \
-  -n openstack cert-manager.io/renew-request-at="$RENEW_AT" --overwrite
-
-
+echo -e "\nWaiting for certificates to get renewed, script will continue after 120 seconds"
 sleep 120s
-
-echo -n "Certificate Validity for cert-triliovault-wlm-public-svc:"
+echo -e "\nFollowing are the new certificate validity period. Please check if they got renewed."
+echo -e "\nCertificate Validity for cert-triliovault-wlm-public-svc:"
 oc get secret cert-triliovault-wlm-public-svc -n openstack -o jsonpath='{.data.tls\.crt}' | base64 -d | openssl x509 -noout -dates
 
 
-echo -n "Certificate Validity for cert-triliovault-wlm-internal-svc:"
+echo -e "\nCertificate Validity for cert-triliovault-wlm-internal-svc:"
 oc get secret cert-triliovault-wlm-internal-svc -n openstack -o jsonpath='{.data.tls\.crt}' | base64 -d | openssl x509 -noout -dates
 
 
-echo -n "Certificate Validity for cert-triliovault-datamover-public-svc:"
+echo -e "\nCertificate Validity for cert-triliovault-datamover-public-svc:"
 oc get secret cert-triliovault-datamover-public-svc -n openstack -o jsonpath='{.data.tls\.crt}' | base64 -d | openssl x509 -noout -dates
 
-echo -n "Certificate Validity for cert-triliovault-datamover-internal-svc "
+echo -e "\nCertificate Validity for cert-triliovault-datamover-internal-svc "
 oc get secret cert-triliovault-datamover-internal-svc  -n openstack -o jsonpath='{.data.tls\.crt}' | base64 -d | openssl x509 -noout -dates
 
-sleep 10s
+
+echo -e "\n\nCheck if certificate validity is updated in above command outputs, if not updated you can stop script here using key ctrl + c"
+echo -e "Script will continue after 30 seconds"
+sleep 30s
 
 
+echo -e "\nCopying cert secrets to trilio-openstack namespace"
 oc get secret cert-triliovault-wlm-public-svc -n openstack -o yaml > cert-triliovault-wlm-public-svc.yaml
 oc get secret cert-triliovault-wlm-internal-svc -n openstack -o yaml > cert-triliovault-wlm-internal-svc.yaml
 
 sed -i 's/openstack/trilio-openstack/' cert-triliovault-wlm-internal-svc.yaml
 sed -i 's/openstack/trilio-openstack/' cert-triliovault-wlm-public-svc.yaml
 
-oc apply -f cert-triliovault-wlm-public-svc.yaml
-oc apply -f cert-triliovault-wlm-internal-svc.yaml
+oc -n trilio-openstack delete secret cert-triliovault-wlm-public-svc 
+oc -n trilio-openstack apply -f cert-triliovault-wlm-public-svc.yaml
+
+oc -n trilio-openstack delete secret cert-triliovault-wlm-internal-svc
+oc -n trilio-openstack apply -f cert-triliovault-wlm-internal-svc.yaml
 
 oc describe secret cert-triliovault-wlm-public-svc -n trilio-openstack
 oc describe secret cert-triliovault-wlm-internal-svc -n trilio-openstack
@@ -58,10 +62,27 @@ oc get secret cert-triliovault-datamover-internal-svc -n openstack -o yaml > cer
 sed -i 's/openstack/trilio-openstack/' cert-triliovault-datamover-internal-svc.yaml
 sed -i 's/openstack/trilio-openstack/' cert-triliovault-datamover-public-svc.yaml
 
-oc apply -f cert-triliovault-datamover-public-svc.yaml
-oc apply -f cert-triliovault-datamover-internal-svc.yaml
+oc -n trilio-openstack delete secret cert-triliovault-datamover-public-svc
+oc -n trilio-openstack apply -f cert-triliovault-datamover-public-svc.yaml
+
+oc -n trilio-openstack delete secret cert-triliovault-datamover-internal-svc
+oc -n trilio-openstack apply -f cert-triliovault-datamover-internal-svc.yaml
 
 oc describe secret cert-triliovault-datamover-public-svc -n trilio-openstack
 oc describe secret cert-triliovault-datamover-internal-svc -n trilio-openstack
 
-echo -n "Certificates renewed, please restart Trilio control plane pods."
+echo -e "\n\nCertificates renewed"
+
+echo -e "\nNow restarting trilio control plane pods"
+oc -n trilio-openstack rollout restart deployment triliovault-datamover-api triliovault-wlm-api triliovault-wlm-cron triliovault-wlm-scheduler triliovault-wlm-workloads
+
+oc rollout status deployment/triliovault-datamover-api -n trilio-openstack --timeout=180s
+oc rollout status deployment/triliovault-wlm-api -n trilio-openstack --timeout=180s
+oc rollout status deployment/triliovault-wlm-cron -n trilio-openstack --timeout=180s
+oc rollout status deployment/triliovault-wlm-scheduler -n trilio-openstack --timeout=180s
+oc rollout status deployment/triliovault-wlm-workloads -n trilio-openstack --timeout=180s
+
+sleep 30s
+oc -n trilio-openstack get pods
+echo -e "\nTrilio control plane pods are up and running with new certificates"
+echo -e "\n Please ignore any pods in 'Terminating' state"
