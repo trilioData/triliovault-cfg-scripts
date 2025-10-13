66e2269dddf499c4f730ebbd3b57bad2980dfe7b
Shruti Anekar <97589399+shruti-trilio@users.noreply.github.com>
2025-10-13 17:19:13 +0530

    TVAULT-6883: Update datamover init scripts to create nova-compute related conf files (#1246)

--- openstack-helm/trilio-openstack/utils/get_admin_creds.sh
+++ openstack-helm/trilio-openstack/utils/get_admin_creds.sh
@@ -30,7 +30,9 @@ MYSQL_DBADMIN_PASSWORD=$(kubectl -n openstack get secrets/mariadb-dbadmin-passwo
 TRILIO_RABBITMQ_ADMIN_PASSWORD=$(kubectl -n trilio-openstack get secret/rabbitmq-default-user --template={{.data.password}} | base64 -d)
 TRILIO_RABBITMQ_ADMIN_USERNAME=$(kubectl -n trilio-openstack get secret/rabbitmq-default-user --template={{.data.username}} | base64 -d)
 
+## Create nova compute related conf file
 kubectl -n openstack get secret/nova-etc -o "jsonpath={.data['nova-compute\.conf']}" | base64 -d > ../templates/bin/_triliovault-nova-compute.conf.tpl
+./sync_nova_compute.sh
 
 CRT=$(kubectl -n openstack get secrets/keystone-tls-public -o "jsonpath={.data['tls\.crt']}" | base64 -d | sed 's/^[ \t]*//' | sed 's/^/            /')
 KEY=$(kubectl -n openstack get secrets/keystone-tls-public -o "jsonpath={.data['tls\.key']}" | base64 -d | awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}')
