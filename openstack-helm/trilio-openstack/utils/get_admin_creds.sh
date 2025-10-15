fa0c7c186fad1fe39c7e77db52e8cce927a37a8b
Shruti Anekar <97589399+shruti-trilio@users.noreply.github.com>
2025-10-15 12:34:29 +0530

    Merge dev-maint5/6.1 into maint/6.1 (#1247)
    * TVAULT-6845: Separate RabbitMQ installation for T4O OS Helm & MOSK
    
    * TVAULT-6870: Mount /etc/localtime in readonly mode in OS Helm
    
    * TVAULT-6883:  create and use dmapi user in rabbitmq & create nova-compute related conf files
    
    ---------
    
    Co-authored-by: Shyam Biradar <shyam.biradar@trilio.io>

--- openstack-helm/trilio-openstack/utils/get_admin_creds.sh
+++ openstack-helm/trilio-openstack/utils/get_admin_creds.sh
@@ -26,13 +26,13 @@ OS_USERNAME=$(kubectl -n openstack get secrets/nova-keystone-admin --template={{
 ## DB creds
 MYSQL_DBADMIN_PASSWORD=$(kubectl -n openstack get secrets/mariadb-dbadmin-password --template={{.data.MYSQL_DBADMIN_PASSWORD}} | base64 -d)
 
-## Rabbitmq creds
-RABBITMQ_ADMIN_PASSWORD=$(kubectl -n openstack get secrets/rabbitmq-admin-user --template={{.data.RABBITMQ_ADMIN_PASSWORD}} | base64 -d)
-RABBITMQ_ADMIN_USERNAME=$(kubectl -n openstack get secrets/rabbitmq-admin-user --template={{.data.RABBITMQ_ADMIN_USERNAME}} | base64 -d)
+## Trilio Rabbitmq 
+TRILIO_RABBITMQ_ADMIN_PASSWORD=$(kubectl -n trilio-openstack get secret/rabbitmq-default-user --template={{.data.password}} | base64 -d)
+TRILIO_RABBITMQ_ADMIN_USERNAME=$(kubectl -n trilio-openstack get secret/rabbitmq-default-user --template={{.data.username}} | base64 -d)
 
-NOVA_TRANSPORT_URL=$(kubectl -n openstack get secret nova-rabbitmq-user --template={{.data.TRANSPORT_URL}} | base64 -d)
-
+## Create nova compute related conf file
 kubectl -n openstack get secret/nova-etc -o "jsonpath={.data['nova-compute\.conf']}" | base64 -d > ../templates/bin/_triliovault-nova-compute.conf.tpl
+./sync_nova_compute.sh
 
 CRT=$(kubectl -n openstack get secrets/keystone-tls-public -o "jsonpath={.data['tls\.crt']}" | base64 -d | sed 's/^[ \t]*//' | sed 's/^/            /')
 KEY=$(kubectl -n openstack get secrets/keystone-tls-public -o "jsonpath={.data['tls\.key']}" | base64 -d | awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}')
@@ -45,12 +45,6 @@ conf:
   wlm: 
     keystone_authtoken:
       memcached_servers: memcached.openstack.svc.$INTERNAL_DOMAIN_NAME:11211
-  datamover:
-    DEFAULT:
-      dmapi_transport_url: $NOVA_TRANSPORT_URL
-  datamover_api:
-    DEFAULT:
-      transport_url: $NOVA_TRANSPORT_URL
 endpoints:
   identity:
     name: keystone
@@ -69,28 +63,28 @@ endpoints:
         host: keystone-api.openstack.svc.$INTERNAL_DOMAIN_NAME
       public:
         host: keystone.$PUBLIC_DOMAIN_NAME
-  oslo_messaging:
+  oslo_messaging_wlm:
     auth:
       admin:
-        username: $RABBITMQ_ADMIN_USERNAME
-        password: $RABBITMQ_ADMIN_PASSWORD
+        username: $TRILIO_RABBITMQ_ADMIN_USERNAME
+        password: $TRILIO_RABBITMQ_ADMIN_PASSWORD
         secret:
           tls:
             internal: rabbitmq-tls-direct
     host_fqdn_override:
       default:
-        host: rabbitmq.openstack.svc.$INTERNAL_DOMAIN_NAME
-  oslo_messaging_nova:
+        host: rabbitmq.trilio-openstack.svc.$INTERNAL_DOMAIN_NAME
+  oslo_messaging_datamover:
     auth:
       admin:
-        username: $RABBITMQ_ADMIN_USERNAME
-        password: $RABBITMQ_ADMIN_PASSWORD
+        username: $TRILIO_RABBITMQ_ADMIN_USERNAME
+        password: $TRILIO_RABBITMQ_ADMIN_PASSWORD
         secret:
           tls:
             internal: rabbitmq-tls-direct
     host_fqdn_override:
       default:
-        host: rabbitmq.openstack.svc.$INTERNAL_DOMAIN_NAME
+        host: rabbitmq.trilio-openstack.svc.$INTERNAL_DOMAIN_NAME
   oslo_db_triliovault_datamover:
     auth:
       admin:
