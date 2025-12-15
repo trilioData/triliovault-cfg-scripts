82f0964e52c98add3d1173d6c1f40f80ca61a6e4
shyam-biradar <shyam.biradar@trilio.io>
2025-12-15 14:50:13 +0530

    Updated image url

--- redhat-director-scripts/rhosp18/ctlplane-scripts/uninstall_tvo_control_plane.sh
+++ redhat-director-scripts/rhosp18/ctlplane-scripts/uninstall_tvo_control_plane.sh
@@ -2,6 +2,4 @@
 
 ## Install TVO Control Plane Services
 oc -n trilio-openstack delete -f ./tvo-operator-inputs.yaml
-oc get csv -n trilio-openstack | grep -E 'rabbitmq-cluster-operator|rabbitmq-messaging-topology-operator' | awk '{print $1}' | xargs -r oc delete csv -n trilio-openstack
 oc delete rabbitmqcluster trilio-rabbitmq-cluster
-
