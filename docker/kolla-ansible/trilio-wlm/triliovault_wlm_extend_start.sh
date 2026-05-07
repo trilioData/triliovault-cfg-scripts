c359797d8d1fa59534d118ca48b27c343fb3f794
shyam-biradar <shyam.biradar@trilio.io>
2026-05-07 15:55:08 +0530

    revert: remove imp compatibility shim from trilio-wlm
    Permanent fix for the imp removal in Python 3.12 belongs in the
    workloadmgr package source, not in deployment scripts.

--- docker/kolla-ansible/trilio-wlm/triliovault_wlm_extend_start.sh
+++ docker/kolla-ansible/trilio-wlm/triliovault_wlm_extend_start.sh
@@ -3,6 +3,8 @@
 # Bootstrap and exit if KOLLA_BOOTSTRAP variable is set. This catches all cases
 # of the KOLLA_BOOTSTRAP variable being set, including empty.
 
+## TODO: Uncomment following code once we get dmapi-dbsync tool
+
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
     alembic --config /etc/triliovault-wlm/triliovault-wlm.conf upgrade head
     exit 0
