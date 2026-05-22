7385365d9717aace4113e8e65a671635860cec6e
shyam-biradar <shyam.biradar@trilio.io>
2026-05-22 10:33:40 +0530

    Cleaned un-necessary directories as they will confuse claude and overall development

--- redhat-director-scripts/rhosp16/scripts/stop_datamover_service.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-systemctl disable tripleo_triliovault_datamover.service
-systemctl stop tripleo_triliovault_datamover.service
-
-umount /var/lib/nova/triliovault-mounts
-ls /var/lib/nova/triliovault-mounts
