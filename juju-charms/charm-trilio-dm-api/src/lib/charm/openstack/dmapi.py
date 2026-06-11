2b207be9d01ca0ea8710bb3bbbd982f506f98d03
shyam-biradar <shyam.biradar@trilio.io>
2026-06-11 15:37:54 +0530

    TVAULT-7361: remove unused wlm_uri and DMS_CLIENT_CONF from restart_map
    DMS client is rendered via custom handler code using db_ep.password(prefix='wlm')
    directly. Adding DMS_CLIENT_CONF to restart_map would cause render_with_interfaces()
    to look for a template named 'client.conf' (basename of path) which does not exist,
    breaking all config rendering with TemplateNotFound.

--- juju-charms/charm-trilio-dm-api/src/lib/charm/openstack/dmapi.py
+++ juju-charms/charm-trilio-dm-api/src/lib/charm/openstack/dmapi.py
@@ -24,7 +24,6 @@ import charmhelpers.core.hookenv as hookenv
 DMAPI_DIR = "/etc/triliovault-datamover"
 DMAPI_CONF = os.path.join(DMAPI_DIR, "triliovault-datamover-api.conf")
 DMAPI_LOG_CONF = os.path.join(DMAPI_DIR, "datamover_api_logging.conf")
-DMS_CLIENT_CONF = "/etc/triliovault-dms/client.conf"
 
 plugins.trilio.make_trilio_handlers()
 
@@ -46,11 +45,6 @@ class DmapiDBAdapter(adapters.DatabaseRelationAdapter):
     def dmapi_uri(self):
         """URI for dmapi DB"""
         return self.get_uri(prefix="dmapi")
-
-    @property
-    def wlm_uri(self):
-        """URI for workloadmgr DB (used by DMS client)"""
-        return self.get_uri(prefix="wlm")
 
 
 class DmapiAdapters(adapters.OpenStackAPIRelationAdapters):
@@ -94,8 +88,7 @@ class DmapiCharm(plugins.TrilioVaultCharm):
 
     # The restart map defines which services should be restarted when a given
     # file changes
-    restart_map = {DMAPI_CONF: services, DMAPI_LOG_CONF: services,
-                   DMS_CLIENT_CONF: services}
+    restart_map = {DMAPI_CONF: services, DMAPI_LOG_CONF: services}
 
     adapters_class = DmapiAdapters
 
