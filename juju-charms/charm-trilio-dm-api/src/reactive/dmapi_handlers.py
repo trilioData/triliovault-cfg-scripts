8ae0a99c4f59fbc7951a276d34e513d6b3226f23
shyam-biradar <shyam.biradar@trilio.io>
2026-06-17 12:04:25 +0530

    TVAULT-7138: render DMAPI DMS client.conf with rabbitmq_url in render_config()
    Populate rabbitmq_url and node_id in DMS client.conf as soon as the amqp
    relation is available, without waiting for wlm-db to connect. The db_url
    is left empty and filled in later by render_dms_client_config() when the
    wlm-db relation becomes available.

--- juju-charms/charm-trilio-dm-api/src/reactive/dmapi_handlers.py
+++ juju-charms/charm-trilio-dm-api/src/reactive/dmapi_handlers.py
@@ -69,6 +69,32 @@ def render_config(*args):
     host.service('disable', 'trilio-dms-server')
     host.service('stop', 'trilio-dms-server')
 
+    # Render DMS client config with rabbitmq_url and node_id from amqp relation.
+    # db_url is left empty here and filled in by render_dms_client_config() when
+    # the wlm-db relation becomes available.
+    amqp = reactive.endpoint_from_flag('amqp.available')
+    transport_url = (
+        f"rabbit://{amqp.username()}:{amqp.password()}"
+        f"@{amqp.private_address()}:{amqp.ssl_port() or 5672}/{amqp.vhost()}"
+    )
+    root_uid = pwd.getpwnam('root').pw_uid
+    nova_gid = grp.getgrnam('nova').gr_gid
+    dms_conf_dir = '/etc/triliovault-dms'
+    os.makedirs(dms_conf_dir, exist_ok=True)
+    os.chown(dms_conf_dir, root_uid, nova_gid)
+    dms_client_conf_path = os.path.join(dms_conf_dir, 'client.conf')
+    render(
+        source='etc_triliovault-dms_client.conf',
+        target=dms_client_conf_path,
+        context={
+            'rabbitmq_url': transport_url,
+            'db_url': '',
+            'node_id': socket.getfqdn(),
+        },
+    )
+    os.chmod(dms_client_conf_path, 0o640)
+    os.chown(dms_client_conf_path, root_uid, nova_gid)
+
     reactive.set_state("config.rendered")
 
 
